fc9dd31e9d5da5a4c19bfb393fd4b91c45b6fe58
Byron Kimani <byronkimani@gmail.com>
2021-02-12 15:49:48 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:messengerUI/screens/conversations.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messenger Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Conversations(),
+    );
+  }
+}
